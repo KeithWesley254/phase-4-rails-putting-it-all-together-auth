0c872b8bc81e4929fa4eca8a100eff9b4bcd5424
Keith <keithwesley187@gmail.com>
2022-10-08 11:29:38 +0300

    milestone: redone

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,13 +8,13 @@ class ApplicationController < ActionController::API
   private
 
   def authorize
-    @current_user = User.find_by(id: session[:user_id])
+    @current_user = User.find_by!(id: session[:user_id])
 
     render json: { errors: ["Not authorized"] }, status: :unauthorized unless @current_user
   end
 
-  def entity_unread(ex)
-    render json: { errors: ex.record.errors.full_messages }, status: :unprocessable_entity
+  def entity_unread(invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
   end
 
 end
